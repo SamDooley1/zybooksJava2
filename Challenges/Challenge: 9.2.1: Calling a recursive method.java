1bc022b4d85a51b3f8be86ff86b6c9132cbcff27
Sam <122411453+SamDooley1@users.noreply.github.com>
2023-06-03 18:49:19 -0700

    Create Challenge: 9.2.1: Calling a recursive method.java

---
+++ Challenges/Challenge: 9.2.1: Calling a recursive method.java
@@ -0,0 +1,26 @@
+//Write a statement that calls the recursive method backwardsAlphabet() with parameter startingLetter.
+
+import java.util.Scanner;
+
+public class RecursiveCalls {
+   public static void backwardsAlphabet(char currLetter) {
+      if (currLetter == 'a') {
+         System.out.println(currLetter);
+      }
+      else {
+         System.out.print(currLetter + " ");
+         backwardsAlphabet((char)(currLetter - 1));
+      }
+   }
+
+   public static void main (String [] args) {
+      Scanner scnr = new Scanner(System.in);
+      char startingLetter;
+
+      startingLetter = scnr.next().charAt(0);
+
+      /* Your solution goes here  */
+      backwardsAlphabet(startingLetter);
+
+   }
+}
